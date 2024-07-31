16419e50a37ac665ad1147f6f3b304471c8ddc9d
DevPBDias <devpbdias@gmail.com>
2024-07-30 22:18:56 -0300

    fix: added new css to make home page responsive and added new timeline

--- src/components/HeroContent.tsx
+++ src/components/HeroContent.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 
 const HeroContent = () => {
   return (
-    <section className="flex flex-col justify-center items-start gap-1 lg:gap-2">
+    <section className="flex flex-col justify-center items-start gap-1 lg:gap-2 lg:w-1/2">
       <p className="lg:text-base text-sm">Olá, sou Paulo Bruno,</p>
-      <h2 className="uppercase text-[var(--secondary)] text-2xl lg:text-6xl font-bold">
+      <h2 className="uppercase text-[var(--secondary)] text-2xl sm:text-4xl 2xl:text-6xl font-bold">
         Desenvolvedor web
       </h2>
-      <p className="lg:text-base text-sm">
+      <p className="2xl:text-base text-sm text-[var(--text-color-terciary)]">
         Apaixonado por soluções criativas e inovaddoras que facilitem a vida
         cotidiana das pessoas.
       </p>
       <Link
         target="_blank"
         rel="noreferrer noopener"
-        className="flex justify-center items-center bg-[var(--secondary)] lg:text-xl font-bold text-[var(--text-color-secondary)] w-full py-1 lg:px-7 lg:py-1 rounded-lg shadow-sm mt-5 ml- lg:mt-9"
+        className="flex justify-center items-center bg-[var(--secondary)] 2xl:text-xl font-bold text-[var(--text-color-secondary)] w-full sm:w-24 py-1 lg:px-7 lg:py-1 rounded-lg shadow-sm mt-5 ml- lg:mt-9"
         href="/CV-Dev-Paulo-Bruno.pdf"
       >
         Ler CV
--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -3,7 +3,7 @@ import hero from "../../public/assets/images/Hero.png";
 
 const HeroImage = () => {
   return (
-    <div className="flex items-center justify-center border-4 border-[var(--secondary)] w-56 h-56 lg:w-96 lg:h-96 rounded-full">
+    <div className="flex items-center justify-center border-4 border-[var(--secondary)] w-56 h-56 2xl:w-96 2xl:h-96 rounded-full">
       <Image
         className="w-full h-full object-cover"
         src={hero}
